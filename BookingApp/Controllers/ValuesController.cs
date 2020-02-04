09b3f7217ed5482e6676fce9fde49c299399ffe2
dilshod.sadiev <dilshod.sadiev@TIBERDT693>
2020-02-04 10:53:36 -0500

    first commit

---
+++ BookingApp/Controllers/ValuesController.cs
@@ -0,0 +1,91 @@
+﻿using BookingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BookingApp.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        BookContext db = new BookContext();
+
+        public IEnumerable<Book> GetBooks()
+        {
+            return db.Books;
+        }
+
+        public Book GetBook(int id)
+        {
+            Book book = db.Books.Find(id);
+            return book;
+        }
+
+        [HttpPost]
+        public void CreateBook([FromBody]Book book)
+        {
+            db.Books.Add(book);
+            db.SaveChanges();
+        }
+
+        [HttpPut]
+        public void EditBook(int id, [FromBody]Book book)
+        {
+            if (id == book.Id)
+            {
+                db.Entry(book).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteBook(int id)
+        {
+            Book book = db.Books.Find(id);
+            if (book != null)
+            {
+                db.Books.Remove(book);
+                db.SaveChanges();
+            }
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        //// GET api/values
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+
+
+        //// GET api/values/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/values
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
